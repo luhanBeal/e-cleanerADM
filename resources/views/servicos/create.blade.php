68352330cb61aed311f9c2114c9d938fc1c3405d
luhanBeal <luhansantanna@gmail.com>
2022-05-02 21:38:07 -0300

    Added masks to all form fields

--- resources/views/servicos/create.blade.php
+++ resources/views/servicos/create.blade.php
@@ -14,3 +14,9 @@
   @include('servicos._form')
 </form>
 @stop
+
+{{-- adminLTE permite adicao de JS(script) --}}
+{{-- OU ADD NA AREA PLUGIN DO ADMINLTE --}}
+{{-- @section('js')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js" integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+@endsection --}}
